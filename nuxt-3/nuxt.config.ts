6c7c75bc98b375f0187f23d0f8402968cbee0160
Ivan Demchuk <ivan.demchuk@gmail.com>
2024-04-05 09:47:24 +0300

    Switch to using Nuxt module

--- nuxt-3/nuxt.config.ts
+++ nuxt-3/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['unplugin-fluent-vue/nuxt'],
+  fluentVue: {
+    external: {
+      locales: ['en', 'fr'],
+      ftlDir: 'translations/',
+    }
+  },
 })
